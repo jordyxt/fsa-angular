f5abf8641e10116db4eca5b8a766668c3f6a44a3
Jordy Arrese <jordyarresehuaman@gmail.com>
2022-06-12 17:04:02 +0200

    Added series create ui #20

---
+++ src/app/modules/home/models/series.model.ts
@@ -0,0 +1,11 @@
+export interface Series {
+  id?: number;
+  title: string;
+  description: string;
+  releaseDate: string;
+  seasons: number;
+  endingDate: string;
+  genreList: string[];
+  trailer: string;
+  poster: string;
+}
