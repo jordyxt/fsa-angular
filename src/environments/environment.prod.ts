9102ab9a9f73de111d3e0a45e12cdd41d41e01b9
Jordy Arrese <jordyarresehuaman@gmail.com>
2022-06-27 22:22:55 +0200

    Changed REST url final slash

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: true,
   NAME: name,
   VERSION: version,
-  REST_FSA: 'https://fsa-spring.herokuapp.com/',
+  REST_FSA: 'https://fsa-spring.herokuapp.com',
 };
